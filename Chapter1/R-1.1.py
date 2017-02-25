78d166998516cee4b4a2c17c371e73554704bde8
Jeremi Podlasek <pepi@jeremis-macbook-pro.home>
2017-02-25 18:06:07 +0100

    Problem 1 & 2 from chapter one Reinforcement

---
+++ Chapter1/R-1.1.py
@@ -0,0 +1,9 @@
+def is_multiple(n,m):
+    if m % n == 0:
+        return True
+    return False
+
+print(is_multiple(2,4))
+print(is_multiple(3,6))
+print(is_multiple(4,2))
+print(is_multiple(6,3))
---
+++ Chapter1/R-1.2.py
@@ -0,0 +1,9 @@
+def is_even(k):
+	if k & 1:
+		return False
+	return True
+
+print(is_even(1))
+print(is_even(2))
+print(is_even(3))
+print(is_even(4))
